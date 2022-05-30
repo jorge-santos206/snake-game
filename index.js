cb9e73507e0b55532810b5131dd2e1d6bab4b580
jorge santos <jorgeantoniosantos27@gmail.com>
2022-05-30 18:32:54 -0300

    criando layout

---
+++ index.js
@@ -0,0 +1,6 @@
+const gameBoard = document.querySelector("#gameboard");
+const ctx = gameBoard.getContext("2d");
+const score = document.querySelector("#score")
+const btn = document.querySelector("#reset")
+const gameWith = gameBoard.width;
+cosnt gameHeight = gameBoard.height;
